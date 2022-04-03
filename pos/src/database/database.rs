e17a9d15f3252df109f63dad72067458ed4f64c1
Jared Dyreson <jared.dyreson@gmail.com>
2022-04-03 11:44:00 -0700

    added connection interface

--- pos/src/database/database.rs
+++ pos/src/database/database.rs
@@ -1,6 +1,18 @@
 extern crate redis;
 use redis::Commands;
 
+pub struct DatabaseConnection {
+    pub _connection: redis::Connection,
+}
+
+impl DatabaseConnection {
+    pub fn new() -> DatabaseConnection {
+        let client  = redis::Client::open("redis://127.0.0.1/").unwrap();
+        let con = client.get_connection().unwrap();
+        DatabaseConnection{ _connection: con }
+    }
+}
+
 pub fn do_something() -> redis::RedisResult<()> {
     let client  = redis::Client::open("redis://127.0.0.1/")?;
     let mut connection = client.get_connection()?;
